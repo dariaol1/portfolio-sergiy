59bf6704fbd3f85768bd6de7f94f93bef8f34cf9
dariaol1 <156906210+dariaol1@users.noreply.github.com>
2024-01-19 12:51:34 +0100

    add project

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function(){
+  const menuEl = document.getElementById("menu")
+  const headercontEl = document.getElementById("header__content")
+
+  window.addEventListener("scroll", function () {
+    const scrollPos = window.scrollY
+
+    if (scrollPos > 90) {
+      menuEl.classList.add("menu--fixed")
+      headercontEl.classList.add("header_content--big")
+    } else {
+      menuEl.classList.remove("menu--fixed")
+      headercontEl.classList.remove("header_content--big")
+    }
+  });
+
+  $(".logo, .menu a, .header__link").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+  
+  $('.burger-btn, .menu a').on('click', function(){
+    $('.burger-btn').toggleClass('burger-btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  var mixer = mixitup('.works__content');
+});
+
